5d504c13fe6dc1f40606df7992d903252d88b968
Catherine Bronzo <cbronzo@mac.com>
2018-09-25 21:49:51 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,5 +19,12 @@ class RecipesController < ApplicationController
   end
   
   def recipe_params
+     params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
